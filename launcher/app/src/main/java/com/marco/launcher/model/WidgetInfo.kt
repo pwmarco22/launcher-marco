9f0bdbc4cbae17d6755c32973d8825258cda8357
pwmarco22 <pwmarco@proton.me>
2026-05-27 08:20:55 +0530

    Add WidgetInfo data class for widget management

---
+++ launcher/app/src/main/java/com/marco/launcher/model/WidgetInfo.kt
@@ -0,0 +1,8 @@
+package com.marco.launcher.model
+
+import android.appwidget.AppWidgetProviderInfo
+
+data class WidgetInfo(
+    val appWidgetId: Int,
+    val providerInfo: AppWidgetProviderInfo
+)
